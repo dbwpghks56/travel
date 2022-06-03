aa51ceedb9cee7df9653509bcc6198e5a68e4e09
JaeHwan <wpghks56@naver.com>
2022-06-03 13:46:24 +0900

    frontdesignpattern

--- src/main/java/travel/controller/SearchServlet.java
+++ src/main/java/travel/controller/SearchServlet.java
@@ -15,20 +15,16 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SearchServlet
  */
-@WebServlet("/jsp/search.do")
+//@WebServlet("/jsp/search.do")
 public class SearchServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			// TODO Auto-generated method stub
-			req.setCharacterEncoding("utf-8");
-			searchContreller controller = new searchContreller();
-			RequestDispatcher rd = req.getRequestDispatcher(controller.execute(req));
-			rd.forward(req, resp);
-			
-		}
-
-
-	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		searchContreller controller = new searchContreller();
+		RequestDispatcher rd = req.getRequestDispatcher(controller.execute(req));
+		rd.forward(req, resp);
+		
+	}
 
 }
---
+++ src/main/java/travel/model/UserService.java
@@ -0,0 +1,27 @@
+package travel.model;
+
+import travel.DTO.UserDTO;
+
+public class UserService {
+	UserDAO userdao = new UserDAO();
+	
+	public int insertUser(UserDTO user) {
+		return userdao.insertUser(user);
+	}
+	
+	public UserDTO loginUser(String user_id, String user_pass) {
+		return userdao.loginUser(user_id, user_pass);
+	}
+	
+	public int confirmUserid(String user_id) {
+		return userdao.confirmUserid(user_id);
+	}
+	
+	public int confirmNickname(String nickname) {
+		return userdao.confirmNickname(nickname);
+	}
+	
+	public int updateUser(String user_id) {
+		return userdao.updateUser(user_id);
+	}
+}
